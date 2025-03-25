c237ba12aab64b299f05a97dd2ac3606db6c8ed7
Maísa Gontijo <165193148+maisagontijoo@users.noreply.github.com>
2025-03-25 10:43:45 -0300

    Add files

---
+++ idade-canina/tercafeiraaula1/ContentView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var humanAge: String = ""
+    @State private var dogAge: String = ""
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Text("Calculadora de Idade Canina")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                TextField("Digite a idade humana", text: $humanAge)
+                    .keyboardType(.numberPad)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .frame(width: 250)
+                
+                Button(action: calculateDogAge) {
+                    Text("Calcular")
+                        .padding()
+                        .frame(width: 150)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Button(action: apagar) {
+                    Text("Apagar")
+                        .padding()
+                        .frame(width: 150)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                if !dogAge.isEmpty {
+                    Text("A idade em anos caninos é: \(dogAge)")
+                        .font(.headline)
+                        .padding()
+                        .foregroundColor(.white)
+                }
+            }
+        }
+    }
+    
+    func calculateDogAge() {
+        if let age = Int(humanAge) {
+            let result = age * 7
+            dogAge = "\(result) anos"
+        } else {
+            dogAge = "Digite um número válido!"
+        }
+    }
+    func apagar() {
+        
+            dogAge = " "
+    }
+}
---
+++ idade-canina/tercafeiraaula1/tercafeiraaula1App.swift
@@ -0,0 +1,17 @@
+//
+//  tercafeiraaula1App.swift
+//  tercafeiraaula1
+//
+//  Created by COTEMIG on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct tercafeiraaula1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
